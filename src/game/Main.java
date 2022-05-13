acd2194de9bc838396ccdf03177594ba09804310
some random idiot <sittipat2554aa@hotmail.com>
2022-05-13 21:24:27 +0700

    Added a menu.

--- src/game/Main.java
+++ src/game/Main.java
@@ -1,8 +1,34 @@
 package game;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
+    private static void initMenu() {
+        JFrame menuWindow = new JFrame("Welcome to TANKS!");
+        menuWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        menuWindow.setSize(500, 500);
+
+        JPanel menuPanel = new JPanel();
+        menuPanel.setLayout(new GridLayout(3, 1));
+
+        JButton singleplayerButton = new JButton("Singleplayer");
+        JButton multiplayerButton = new JButton("Multiplayer");
+        JButton exitButton = new JButton("Exit");
+
+        menuPanel.add(singleplayerButton);
+        menuPanel.add(multiplayerButton);
+        menuPanel.add(exitButton);
+
+        menuWindow.add(menuPanel);
+        menuWindow.setVisible(true);
+    }
+
     public static void main(String[] args) {
+        initMenu();
+
         Director director = new Director();
         director.start();
+
     }
 }
