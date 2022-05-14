04490b762e1c323c1fd1833d6fe50088849274c9
some random idiot <sittipat2554aa@hotmail.com>
2022-05-14 10:48:00 +0700

    Added a flyweight class for tank sprites.

---
+++ src/tanks/TankSprites.java
@@ -0,0 +1,17 @@
+package tanks;
+
+import javax.swing.*;
+
+public class TankSprites {
+    public static ImageIcon player1SpriteUp = new ImageIcon("./asset/sprite/playerTankAUp.png");
+    public static ImageIcon player1SpriteDown = new ImageIcon("./asset/sprite/playerTankADown.png");
+    public static ImageIcon player1SpriteLeft = new ImageIcon("./asset/sprite/playerTankALeft.png");
+    public static ImageIcon player1SpriteRight = new ImageIcon("./asset/sprite/playerTankARight.png");
+
+    public static ImageIcon player2SpriteUp = new ImageIcon("./asset/sprite/playerTankBUp.png");
+    public static ImageIcon player2SpriteDown = new ImageIcon("./asset/sprite/playerTankBDown.png");
+    public static ImageIcon player2SpriteLeft = new ImageIcon("./asset/sprite/playerTankBLeft.png");
+    public static ImageIcon player2SpriteRight = new ImageIcon("./asset/sprite/playerTankBRight.png");
+
+    public static ImageIcon botSprite = new ImageIcon("./asset/sprite/botTank.png");
+}
