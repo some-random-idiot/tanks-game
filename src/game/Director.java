97e220b4bca9dcdd9930380aca26246fdb971ebf
some random idiot <sittipat2554aa@hotmail.com>
2022-05-15 09:49:25 +0700

    Changed Director class into a singleton.

--- src/game/Director.java
+++ src/game/Director.java
@@ -1,14 +1,28 @@
 package game;
 
+import tanks.TankShellPool;
+
 import java.util.Observable;
 import java.util.Observer;
 
 public class Director implements Observer {
+    public static Director directorInstance;
     public static int frameRate = 1000 / 60;
     public World world;
+    public boolean onGoing = false;
+    public TankShellPool tankShellPool;
 
     public void start(String mode) {
+        onGoing = true;
         world = new World(mode);
+        tankShellPool = new TankShellPool();
+    }
+
+    public static Director getInstance() {
+        if (directorInstance == null) {
+            directorInstance = new Director();
+        }
+        return directorInstance;
     }
 
     @Override
