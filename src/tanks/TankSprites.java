cce4be2654a5d4ff28716aa28f7ced04e01d1fd8
some random idiot <sittipat2554aa@hotmail.com>
2022-05-16 20:19:38 +0700

    Moved sprites into src folder for easy JAR compilation.

--- src/tanks/TankSprites.java
+++ src/tanks/TankSprites.java
@@ -3,23 +3,23 @@ package tanks;
 import javax.swing.*;
 
 public class TankSprites {
-    public static ImageIcon player1SpriteUp = new ImageIcon("./asset/sprite/playerTankAUp.png");
-    public static ImageIcon player1SpriteDown = new ImageIcon("./asset/sprite/playerTankADown.png");
-    public static ImageIcon player1SpriteLeft = new ImageIcon("./asset/sprite/playerTankALeft.png");
-    public static ImageIcon player1SpriteRight = new ImageIcon("./asset/sprite/playerTankARight.png");
+    public static ImageIcon player1SpriteUp = new ImageIcon(TankSprites.class.getResource("/asset/sprite/playerTankAUp.png"));
+    public static ImageIcon player1SpriteDown = new ImageIcon(TankSprites.class.getResource("/asset/sprite/playerTankADown.png"));
+    public static ImageIcon player1SpriteLeft = new ImageIcon(TankSprites.class.getResource("/asset/sprite/playerTankALeft.png"));
+    public static ImageIcon player1SpriteRight = new ImageIcon(TankSprites.class.getResource("/asset/sprite/playerTankARight.png"));
 
-    public static ImageIcon player2SpriteUp = new ImageIcon("./asset/sprite/playerTankBUp.png");
-    public static ImageIcon player2SpriteDown = new ImageIcon("./asset/sprite/playerTankBDown.png");
-    public static ImageIcon player2SpriteLeft = new ImageIcon("./asset/sprite/playerTankBLeft.png");
-    public static ImageIcon player2SpriteRight = new ImageIcon("./asset/sprite/playerTankBRight.png");
+    public static ImageIcon player2SpriteUp = new ImageIcon(TankSprites.class.getResource("/asset/sprite/playerTankBUp.png"));
+    public static ImageIcon player2SpriteDown = new ImageIcon(TankSprites.class.getResource("/asset/sprite/playerTankBDown.png"));
+    public static ImageIcon player2SpriteLeft = new ImageIcon(TankSprites.class.getResource("/asset/sprite/playerTankBLeft.png"));
+    public static ImageIcon player2SpriteRight = new ImageIcon(TankSprites.class.getResource("/asset/sprite/playerTankBRight.png"));
 
-    public static ImageIcon botSpriteUp = new ImageIcon("./asset/sprite/botTankUp.png");
-    public static ImageIcon botSpriteDown = new ImageIcon("./asset/sprite/botTankDown.png");
-    public static ImageIcon botSpriteLeft = new ImageIcon("./asset/sprite/botTankLeft.png");
-    public static ImageIcon botSpriteRight = new ImageIcon("./asset/sprite/botTankRight.png");
+    public static ImageIcon botSpriteUp = new ImageIcon(TankSprites.class.getResource("/asset/sprite/botTankUp.png"));
+    public static ImageIcon botSpriteDown = new ImageIcon(TankSprites.class.getResource("/asset/sprite/botTankDown.png"));
+    public static ImageIcon botSpriteLeft = new ImageIcon(TankSprites.class.getResource("/asset/sprite/botTankLeft.png"));
+    public static ImageIcon botSpriteRight = new ImageIcon(TankSprites.class.getResource("/asset/sprite/botTankRight.png"));
 
-    public static ImageIcon shellSpriteUp = new ImageIcon("./asset/sprite/shellUp.png");
-    public static ImageIcon shellSpriteDown = new ImageIcon("./asset/sprite/shellDown.png");
-    public static ImageIcon shellSpriteLeft = new ImageIcon("./asset/sprite/shellLeft.png");
-    public static ImageIcon shellSpriteRight = new ImageIcon("./asset/sprite/shellRight.png");
+    public static ImageIcon shellSpriteUp = new ImageIcon(TankSprites.class.getResource("/asset/sprite/shellUp.png"));
+    public static ImageIcon shellSpriteDown = new ImageIcon(TankSprites.class.getResource("/asset/sprite/shellDown.png"));
+    public static ImageIcon shellSpriteLeft = new ImageIcon(TankSprites.class.getResource("/asset/sprite/shellLeft.png"));
+    public static ImageIcon shellSpriteRight = new ImageIcon(TankSprites.class.getResource("/asset/sprite/shellRight.png"));
 }
--- src/tile/TileSprites.java
+++ src/tile/TileSprites.java
@@ -3,8 +3,8 @@ package tile;
 import javax.swing.*;
 
 public class TileSprites {
-    public static ImageIcon brickSprite = new ImageIcon("./asset/sprite/brick.png");
-    public static ImageIcon steelSprite = new ImageIcon("./asset/sprite/steel.png");
-    public static ImageIcon treeSprite = new ImageIcon("./asset/sprite/tree.png");
-    public static ImageIcon baseSprite = new ImageIcon("./asset/sprite/base.png");
+    public static ImageIcon brickSprite = new ImageIcon(TileSprites.class.getResource("/asset/sprite/brick.png"));
+    public static ImageIcon steelSprite = new ImageIcon(TileSprites.class.getResource("/asset/sprite/steel.png"));
+    public static ImageIcon treeSprite = new ImageIcon(TileSprites.class.getResource("/asset/sprite/tree.png"));
+    public static ImageIcon baseSprite = new ImageIcon(TileSprites.class.getResource("/asset/sprite/base.png"));
 }
